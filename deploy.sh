f803774e574184c8e0a4d02eb2bf35bd4974823f
Seidazimov <nurgali_2994@mail.ru>
2025-03-16 16:50:50 +0500

    test

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,11 @@ chmod -R u+rwX,g+rwX,o+rwX /var/www/bootstrap/cache
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 cp .env.example .env
 docker-compose up -d
-docker exec Laravel11-php-fpm composer install --no-dev --optimize-autoloader
+docker exec Laravel11-php-fpm composer install --optimize-autoloader
 docker exec Laravel11-php-fpm php artisan key:generate
-docker exec Laravel11-php-fpm php artisan migrate --seed
+docker exec Laravel11-php-fpm php artisan migrate:fresh --seed
 docker exec Laravel11-php-fpm php artisan optimize
 docker exec Laravel11-php-fpm /etc/init.d/supervisor start
-docker exec Laravel11-node npm run install
+docker exec Laravel11-node npm install
 docker exec Laravel11-node npm run build
 echo "Done!"
